fa430aa29d40ad5062e8d06fa5bc37784c6f4799
pkang13 <pingyeo13@gmail.com>
2017-06-09 19:49:38 +0900

    Create layout.js

--- m.layouts/colorCode/js/layout.js
+++ m.layouts/colorCode/js/layout.js
@@ -2,10 +2,13 @@ jQuery(function($){
     var lang = $('.link .lang');
     var lang_lst = $('.lang .lang_lst')
     lang.click(function(){
-        if(lang.hasClass('on')){
+        if(lang.hasClass('on'))
+		{
             lang.removeClass('on').addClass('off');
             lang_lst.hide();
-        }else if(lang.hasClass('off')){
+        }
+		else if(lang.hasClass('off'))
+		{
             lang.removeClass('off').addClass('on');
             lang_lst.show();
         }
